a4205c1f7ce41b466e8ee6b69f196d86fc8ede2b
lucible <gobbldygook@letterboxes.org>
2021-02-24 18:59:40 -0600

    updated to work with alexis' new reop

--- sheet_values.py
+++ sheet_values.py
@@ -9,19 +9,23 @@ VersionAdded = ['1.0.0',
                 '1.2.0',
                 '1.3.0',
                 '1.4.0',
-                '1.5.0']
+                '1.5.0',
+                '1.6.0',
+                '1.7.0',
+                '1.8.0',
+                '1.9.0']
 
 def stripString(df):
     df['label'] = df['label'].map(lambda x: re.sub(r'[a-zA-Z_]*', '', x))
-    df['text'] = df['text'].map(lambda x: x.strip(' 	2\\0Ā촃Ȁ촃Ѐ촃촀'))
     return df
 
 def getItemStrings():
-    path = 'data/game_parsed/acnh-message-usen/String_USen/Item'
-    items = ['STR_ItemName_00_Ftr.csv', 'STR_ItemName_01_Art.csv', 'STR_ItemName_15_Cap.csv', 'STR_ItemName_19_Umbrella.csv', 'STR_ItemName_20_Tool.csv', 'STR_ItemName_30_Insect.csv', 'STR_ItemName_31_Fish.csv', 'STR_ItemName_32_DiveFish.csv', 'STR_ItemName_33_Shell.csv', 'STR_ItemName_34_Fossil.csv', 'STR_ItemName_36_InsectToy.csv', 'STR_ItemName_37_FishToy.csv', 'STR_ItemName_40_Plant.csv', 'STR_ItemName_41_Turnip.csv', 'STR_ItemName_50_RoomWall.csv', 'STR_ItemName_51_RoomFloor.csv', 'STR_ItemName_52_RoomRug.csv', 'STR_ItemName_61_HouseDoorDeco.csv', 'STR_ItemName_62_HousePost.csv', 'STR_ItemName_70_Craft.csv', 'STR_ItemName_80_Etc.csv', 'STR_ItemName_81_Event.csv', 'STR_ItemName_82_Music.csv', 'STR_ItemName_83_Fence.csv', 'STR_ItemName_84_Bromide.csv', 'STR_ItemName_85_BridgeSlope.csv', 'STR_ItemName_86_Poster.csv', 'STR_ItemName_90_Money.csv', 'STR_ItemName_91_PhotoStudioList.csv']
+    path = 'data/game_parsed/acnh-strings-usen/Item'
+    # items = ['STR_ItemName_00_Ftr.csv', 'STR_ItemName_01_Art.csv', 'STR_ItemName_19_Umbrella.csv', 'STR_ItemName_20_Tool.csv', 'STR_ItemName_30_Insect.csv', 'STR_ItemName_31_Fish.csv', 'STR_ItemName_32_DiveFish.csv', 'STR_ItemName_33_Shell.csv', 'STR_ItemName_34_Fossil.csv', 'STR_ItemName_36_InsectToy.csv', 'STR_ItemName_37_FishToy.csv', 'STR_ItemName_40_Plant.csv', 'STR_ItemName_41_Turnip.csv', 'STR_ItemName_50_RoomWall.csv', 'STR_ItemName_51_RoomFloor.csv', 'STR_ItemName_52_RoomRug.csv', 'STR_ItemName_61_HouseDoorDeco.csv', 'STR_ItemName_62_HousePost.csv', 'STR_ItemName_70_Craft.csv', 'STR_ItemName_80_Etc.csv', 'STR_ItemName_81_Event.csv', 'STR_ItemName_82_Music.csv', 'STR_ItemName_83_Fence.csv', 'STR_ItemName_84_Bromide.csv', 'STR_ItemName_85_BridgeSlope.csv', 'STR_ItemName_86_Poster.csv', 'STR_ItemName_90_Money.csv', 'STR_ItemName_91_PhotoStudioList.csv']
+    items = ['STR_ItemName_00_Ftr.csv', 'STR_ItemName_01_Art.csv', 'STR_ItemName_19_Umbrella.csv', 'STR_ItemName_20_Tool.csv', 'STR_ItemName_30_Insect.csv', 'STR_ItemName_31_Fish.csv', 'STR_ItemName_32_DiveFish.csv', 'STR_ItemName_33_Shell.csv', 'STR_ItemName_34_Fossil.csv', 'STR_ItemName_36_InsectToy.csv', 'STR_ItemName_37_FishToy.csv', 'STR_ItemName_40_Plant.csv', 'STR_ItemName_41_Turnip.csv', 'STR_ItemName_50_RoomWall.csv', 'STR_ItemName_51_RoomFloor.csv', 'STR_ItemName_52_RoomRug.csv', 'STR_ItemName_61_HouseDoorDeco.csv', 'STR_ItemName_62_HousePost.csv', 'STR_ItemName_70_Craft.csv', 'STR_ItemName_80_Etc.csv', 'STR_ItemName_81_Event.csv', 'STR_ItemName_82_Music.csv', 'STR_ItemName_83_Fence.csv', 'STR_ItemName_84_Bromide.csv', 'STR_ItemName_85_BridgeSlope.csv', 'STR_ItemName_86_Poster.csv']
 
     # Get object name strings
-    itemSTR = pd.concat([pd.read_csv(f'{path}/{file}', dtype=object) for file in items])
+    itemSTR = pd.concat([pd.read_csv(f'{path}/{file}', dtype=object, sep=';') for file in items])
 
     # Remove object plural strings
     itemSTR = itemSTR[~itemSTR['label'].str.endswith('_pl').fillna('')]
@@ -33,16 +37,16 @@ def getItemStrings():
     itemSTR = itemSTR.astype({'label': 'int64'})
     
     # Rename columns
-    itemSTR.rename(columns={'text': 'Name_Items', 'label': 'Internal ID'}, inplace=True)
+    itemSTR.rename(columns={'English': 'Name_Items', 'label': 'Internal ID'}, inplace=True)
 
     return itemSTR
 
 def getClothingStrings():
-    path = 'data/game_parsed/acnh-message-usen/String_USen/Outfit/GroupName'
+    path = 'data/game_parsed/acnh-strings-usen/Outfit/GroupName'
     clothing = ['STR_OutfitGroupName_Accessory.csv', 'STR_OutfitGroupName_Bag.csv', 'STR_OutfitGroupName_Bottoms.csv', 'STR_OutfitGroupName_Cap.csv', 'STR_OutfitGroupName_Helmet.csv', 'STR_OutfitGroupName_MarineSuit.csv', 'STR_OutfitGroupName_OnePiece.csv', 'STR_OutfitGroupName_Shoes.csv', 'STR_OutfitGroupName_Socks.csv', 'STR_OutfitGroupName_Tops.csv']
 
     # Get clothing name strings
-    clothingSTR = pd.concat([pd.read_csv(f'{path}/{file}', dtype=object) for file in clothing])
+    clothingSTR = pd.concat([pd.read_csv(f'{path}/{file}', dtype=object, sep=';') for file in clothing])
     
     # Strip label to ID only and remove yucky chars from name string
     clothingSTR = stripString(clothingSTR)
@@ -51,7 +55,7 @@ def getClothingStrings():
     clothingSTR = clothingSTR.astype({'label': 'int64'})
     
     # Rename column 'text' to 'Name'
-    clothingSTR.rename(columns={'text': 'Name_Clothing', 'label' : 'ClothGroup ID'}, inplace=True)
+    clothingSTR.rename(columns={'English': 'Name_Clothing', 'label' : 'ClothGroup ID'}, inplace=True)
     
     return clothingSTR
 
